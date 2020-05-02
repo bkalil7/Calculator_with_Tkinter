4591010919165de5775c910a8ffcbd355faaf396
Bruno Kalil <60933160+bkalil7@users.noreply.github.com>
2020-05-02 16:41:15 -0300

    Started to add some scientific functions and changed the colors

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ from tkinter import *;
 window = Tk();
 
 #Defining the size of the window, width = 312 and height = 324
-window.geometry("312x324");
+window.geometry("400x330");
 
 #Preventing the window from getting resized
 window.resizable(0, 0);
@@ -15,7 +15,7 @@ window.title("Calculator");
 
 #1. Function to update the displayer whenever a button is clicked. Basically a button updater
 def button_click(item):
-    operations = ["/", "+", "-", "*"];
+    operations = ["/", "+", "-", "*", "**", "**2", "%", "**(1/2)"];
     global expression
     global result
     if expression == result and (item in operations):
@@ -58,37 +58,44 @@ input_field.grid(row = 0, column = 0);
 input_field.pack(ipady = 10)# 'ipady' is an internal padding to increase the height of input field
 
 #Frame for the buttons below the input_frame
-buttons_frame = Frame(window, width = 312, height = 272.5, bg = "grey");
+buttons_frame = Frame(window, width = 322, height = 272.5, bg = "grey");
 buttons_frame.pack();
 
 #Setting the buttons
 #First row
-clear = Button(buttons_frame, text = "C", width = 10, height = 3, command = lambda: button_clear()).grid(row = 0, column = 0)
-parenthesis1 = Button(buttons_frame, text = "(", width = 10, height = 3, command = lambda: button_click("(")).grid(row = 0, column = 1)
-parenthesis2 = Button(buttons_frame, text = ")", width = 10, height = 3, command = lambda: button_click(")")).grid(row = 0, column = 2)
-divide = Button(buttons_frame, text = "/", width = 10, height = 3, command = lambda: button_click("/")).grid(row = 0, column = 3)
+percentage = Button(buttons_frame, text = "%", width = 10, height = 3, command = lambda: button_click("/100"), bg = 'black', fg = 'white').grid(row = 0, column = 0)
+clear = Button(buttons_frame, text = "C", width = 10, height = 3, command = lambda: button_clear()).grid(row = 0, column = 1)
+parenthesis1 = Button(buttons_frame, text = "(", width = 10, height = 3, command = lambda: button_click("(")).grid(row = 0, column = 2)
+parenthesis2 = Button(buttons_frame, text = ")", width = 10, height = 3, command = lambda: button_click(")")).grid(row = 0, column = 3)
+divide = Button(buttons_frame, text = "/", width = 10, height = 3, command = lambda: button_click("/"), bg = 'orange').grid(row = 0, column = 4)
+
 
 #Second row
-seven = Button(buttons_frame, text = "7", width = 10, height = 3, command = lambda: button_click("7")).grid(row = 1, column = 0)
-eight = Button(buttons_frame, text = "8", width = 10, height = 3, command = lambda: button_click("8")).grid(row = 1, column = 1)
-nine = Button(buttons_frame, text = "9", width = 10, height = 3, command = lambda: button_click("9")).grid(row = 1, column = 2)
-multiply = Button(buttons_frame, text = "*", width = 10, height = 3, command = lambda: button_click("*")).grid(row = 1, column = 3)
+power2 = Button(buttons_frame, text = "**2", width = 10, height = 3, command = lambda: button_click("**2"), bg = "black", fg = "white").grid(row = 1, column = 0)
+seven = Button(buttons_frame, text = "7", width = 10, height = 3, command = lambda: button_click("7")).grid(row = 1, column = 1)
+eight = Button(buttons_frame, text = "8", width = 10, height = 3, command = lambda: button_click("8")).grid(row = 1, column = 2)
+nine = Button(buttons_frame, text = "9", width = 10, height = 3, command = lambda: button_click("9")).grid(row = 1, column = 3)
+multiply = Button(buttons_frame, text = "x", width = 10, height = 3, command = lambda: button_click("*"), bg = 'orange').grid(row = 1, column = 4)
 
 #Third row
-four = Button(buttons_frame, text = "4", width = 10, height = 3, command = lambda: button_click("4")).grid(row = 2, column = 0)
-five = Button(buttons_frame, text = "5", width = 10, height = 3, command = lambda: button_click("5")).grid(row = 2, column = 1)
-six = Button(buttons_frame, text = "6", width = 10, height = 3, command = lambda: button_click("6")).grid(row = 2, column = 2)
-subtract = Button(buttons_frame, text = "-", width = 10, height = 3, command = lambda: button_click("-")).grid(row = 2, column = 3)
+power = Button(buttons_frame, text = "**", width = 10, height = 3, command = lambda: button_click("**"), bg = 'black', fg = "white").grid(row = 2, column = 0)
+four = Button(buttons_frame, text = "4", width = 10, height = 3, command = lambda: button_click("4")).grid(row = 2, column = 1)
+five = Button(buttons_frame, text = "5", width = 10, height = 3, command = lambda: button_click("5")).grid(row = 2, column = 2)
+six = Button(buttons_frame, text = "6", width = 10, height = 3, command = lambda: button_click("6")).grid(row = 2, column = 3)
+subtract = Button(buttons_frame, text = "-", width = 10, height = 3, command = lambda: button_click("-"), bg = 'orange').grid(row = 2, column = 4)
 
 #Fourth row
-one = Button(buttons_frame, text = "1", width = 10, height = 3, command = lambda: button_click("1")).grid(row = 3, column = 0)
-two = Button(buttons_frame, text = "2", width = 10, height = 3, command = lambda: button_click("2")).grid(row = 3, column = 1)
-three = Button(buttons_frame, text = "3", width = 10, height = 3, command = lambda: button_click("3")).grid(row = 3, column = 2)
-add = Button(buttons_frame, text = "+", width = 10, height = 3, command = lambda: button_click("+")).grid(row = 3, column = 3)
+sqrt = Button(buttons_frame, text = "**1/2", width = 10, height = 3, command = lambda: button_click("**(1/2)"), bg = 'black', fg = "white").grid(row = 3, column = 0)
+one = Button(buttons_frame, text = "1", width = 10, height = 3, command = lambda: button_click("1")).grid(row = 3, column = 1)
+two = Button(buttons_frame, text = "2", width = 10, height = 3, command = lambda: button_click("2")).grid(row = 3, column = 2)
+three = Button(buttons_frame, text = "3", width = 10, height = 3, command = lambda: button_click("3")).grid(row = 3, column = 3)
+add = Button(buttons_frame, text = "+", width = 10, height = 3, command = lambda: button_click("+"), bg = 'orange').grid(row = 3, column = 4)
 
 #Fifth row
-zero = Button(buttons_frame, text = "0", width = 21, height = 3, command = lambda: button_click("0")).grid(row = 4, column = 0, columnspan = 2)
-comma = Button(buttons_frame, text = ",", width = 10, height = 3, command = lambda: button_click(",")).grid(row = 4, column = 2)
-equal = Button(buttons_frame, text = "=", width = 10, height = 3, command = lambda: button_equal()).grid(row = 4, column = 3)
+root = Button(buttons_frame, text = "x**(1/y)", width = 10, height = 3, command = lambda: button_click("**(1/"), bg = 'black', fg = "white").grid(row = 4, column = 0)
+zero = Button(buttons_frame, text = "0", width = 21, height = 3, command = lambda: button_click("0")).grid(row = 4, column = 1, columnspan = 2)
+comma = Button(buttons_frame, text = ",", width = 10, height = 3, command = lambda: button_click(",")).grid(row = 4, column = 3)
+equal = Button(buttons_frame, text = "=", width = 10, height = 3, command = lambda: button_equal(), bg = 'orange').grid(row = 4, column = 4)
+
 
 window.mainloop()
